6e1f2130ddb3f927e66e27e78205b20a0a94b73b
timothecht <timothe.chauvet@efrei.net>
2020-12-18 10:25:13 +0100

    karma browser set to phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['phantomjs'],
     singleRun: false,
     restartOnFileChange: true
   });
